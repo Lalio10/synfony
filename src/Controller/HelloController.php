d1564a6f4092249a9096fcb57bca9cb22544a0e8
Rayan <rayan.zermani@etudiant.univ-reims.fr>
2020-12-15 10:47:59 +0100

    9.2.3 sucession de hello en couleur et avec la première lettre en majuscule

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -14,4 +14,16 @@ class HelloController extends AbstractController
     {
         return $this->render('hello/hello.html.twig', ['name' => $name]);
     }
+
+    /**
+     * @Route("/hello/{name}/{times}")
+     */
+    public function helloManyTime(string $name, int $times = 3)
+    {
+        if ($times <= 0 || $times > 10) {
+            $times = 3;
+        }
+
+        return $this->render('hello/hellomanytimes.html.twig', ['name' => $name, 'times' => $times]);
+    }
 }
