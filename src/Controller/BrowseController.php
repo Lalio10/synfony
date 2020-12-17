35cd0739e03fb5a351f05d98d076982ba717f168
Rayan <rayan.zermani@etudiant.univ-reims.fr>
2020-12-17 20:34:54 +0100

    commit fin de séance

---
+++ src/Controller/BrowseController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Album;
+use App\Entity\Artist;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BrowseController extends AbstractController
+{
+    /**
+     * @Route("/artists")
+     */
+    public function artists(): Response
+    {
+        $artists = $this->getDoctrine()
+            ->getRepository(Artist::class)
+            ->findBy([], ['name' => 'ASC']);
+
+        return $this->render('browse/artists.html.twig', [
+            'artists' => $artists,
+        ]);
+    }
+
+    /**
+     * @Route("/albums/{artistId}")
+     */
+    public function albums(int $artistId): Response
+    {
+        $artist = $this->getDoctrine()
+            ->getRepository(Artist::class)
+            ->find($artistId);
+
+        return $this->render('browse/albums.html.twig', [
+            'artist' => $artist,
+        ]);
+    }
+
+    /**
+     * @Route("/tracks/{id}", name="tracks")
+     * @Entity("album", expr="repository.findWithTracksAndSongs(id)")
+     */
+    public function tracks(Album $album): Response
+    {
+        return $this->render('browse/tracks.html.twig', [
+            'album' => $album,
+        ]);
+    }
+}
--- src/Entity/Artist.php
+++ src/Entity/Artist.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * Artist
+ * Artist.
  *
  * @ORM\Table(name="artist")
  * @ORM\Entity
@@ -19,14 +21,72 @@ class Artist
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="IDENTITY")
      */
-    private $id;
+    private $id = 3;
 
     /**
      * @var string
      *
      * @ORM\Column(name="name", type="string", length=200, nullable=false, options={"default"="''","comment"="Nom"})
      */
-    private $name = '\'\'';
+    private $name = '';
+    /**
+     * @var Collection
+     *
+     * @ORM\OneToMany(targetEntity="Album", mappedBy="artist")
+     * @ORM\OrderBy({"year" = "ASC", "name" = "ASC"})
+     */
+    private $albums;
 
+    public function __construct()
+    {
+        $this->albums = new ArrayCollection();
+    }
 
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Album[]
+     */
+    public function getAlbums(): Collection
+    {
+        return $this->albums;
+    }
+
+    public function addAlbum(Album $album): self
+    {
+        if (!$this->albums->contains($album)) {
+            $this->albums[] = $album;
+            $album->setArtist($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAlbum(Album $album): self
+    {
+        if ($this->albums->removeElement($album)) {
+            // set the owning side to null (unless already changed)
+            if ($album->getArtist() === $this) {
+                $album->setArtist(null);
+            }
+        }
+
+        return $this;
+    }
 }
